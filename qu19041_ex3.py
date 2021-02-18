aaa9c6687c5a0e55f182c8d4132c984f862aa942
Samuel Fish <fishsamuel0@gmail.com>
2021-02-18 23:25:31 +0000

    finished thing pending review

--- qu19041_ex3.py
+++ qu19041_ex3.py
@@ -3,48 +3,74 @@ import matplotlib.pyplot as pyplot
 
 # PART (A)
 
-# Here we first define the function to be integrated followed by the function to evaluate the 
-# integral for conditions c.
-def integrand(x_screen, x_aperture, c):
-    return np.exp( c['waveno'] * 1j * (x_screen - x_aperture)**2 / (2*c['screendist']) )
+def integrand(screen_coord, apt_coord, c):
+    return np.exp( c['waveno'] * 1j * (screen_coord - apt_coord) ** 2 / (2 * c['screendist']) )
 
-def fresnel_integral(x_screen, c):
-
+def fresnel_integral(screen_coord, apt, c):
     assert c['intervals'] % 2 == 0, "number of intervals must be even."
 
-    x_aperture = np.linspace(c['xapt_lower'], c['xapt_upper'], c['intervals']+1)
-    dx_aperture = x_aperture[1] - x_aperture[0]
-    sum_vals = np.zeros( len(x_aperture) )
+    aperture = np.linspace(apt['apt_lower'], apt['apt_upper'], c['intervals'] + 1)
+    d_aperture = aperture[1] - aperture[0]
+    sum_vals = np.zeros( len(aperture), dtype=np.complex_ )
+    
+    for i in range( len(aperture) ):
+        sum_vals[i] = integrand(screen_coord, aperture[i], c)
 
-    for i in range( len(x_aperture) ):
-        sum_vals[i] = integrand(x_screen[i], x_aperture[i], c)
+    return c['multiplier'] * d_aperture/3 * \
+    ( sum_vals[0] + np.sum(sum_vals[0:-1:2] + 4*sum_vals[1::2] + 2*sum_vals[2::2]))
+#   -2*sum_vals[0:-1:2])) which values are getting picked
 
-    return c['multiplier'] * dx_aperture/3 * \
-           ( sum_vals[0] + np.sum(sum_vals[len(x_aperture)] + 4*sum_vals[1::2] + 2*sum_vals[2::2]) )
-
-c = {
-    'xapt_lower': -1e-5, 
-    'xapt_upper': 1e-5,
-    'waveno': 2 * np.pi / 1e-6,
-    'screendist': 0.02,
-    'intervals': 100,
-    'E_0': 1
-    }
-
-c['multiplier'] = c['waveno'] * c['E_0'] / (2 * np.pi * c['screendist'])
-
-x_screen = np.linspace(-5e-3, 5e-3, c['intervals'])
-
-def plotdata(x_screen, c):
-    intensity = np.zeros( len(x_screen) ) 
+def plotdata(screenarray, apt, c):
+    intensity = np.zeros( len(screenarray) ) 
 
     for i in range( len(x_screen) ):
-        intensity[i] = ( abs(fresnel_integral(x_screen[i], c)) )**2
-    
+        intensity[i] = ( abs(fresnel_integral(x_screen[i], apt, c)) ) ** 2
     return intensity
-intensitydata = plotdata(x_screen, c)
-pyplot.plot(intensitydata, x_screen)
+
+constants = {
+    'waveno': 2 * np.pi / 1e-6,
+    'screendist': 0.02,
+    'intervals': 150,
+    'E_0': 5.0
+}
+
+xapt = {
+     'apt_lower': -1e-5, 
+     'apt_upper': 1e-5,
+}
+
+constants['multiplier'] = constants['waveno'] * constants['E_0'] \
+/(2 * np.pi * constants['screendist'])
+
+x_screen = np.linspace(-5e-3, 5e-3, constants['intervals'] + 1)
+
+pyplot.plot(x_screen, plotdata(x_screen, xapt, constants ))
 pyplot.xlabel('Screen coordinate (m)')
 pyplot.ylabel('Relative intensity')
 pyplot.show()
-pyplot.savefig('1d_intensity.png')
+
+# PART (C)
+
+# dictionary for aperture in y direction
+yapt = {
+    'apt_lower': -1e-5,
+    'apt_upper': 1e-5,
+}
+
+constants['multiplier'] = constants['waveno'] * constants['E_0'] \
+/(2 * np.pi * constants['screendist'])
+
+intensity = np.zeros( (constants['intervals'], constants['intervals']))
+    
+y_screen = np.linspace(-5e-3, 5e-3, constants['intervals'] + 1)
+
+for i in range(constants['intervals']):
+    for j in range(constants['intervals']):
+        intensity[i, j] = abs(fresnel_integral(x_screen[i], xapt, constants) *
+                          fresnel_integral(y_screen[j], yapt, constants) \
+                          / constants['multiplier']) ** 2 
+
+pyplot.set_cmap("pink")
+pyplot.imshow(intensity)
+pyplot.show()
+
